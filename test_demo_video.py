abcd56b118cf1b68fded178324783ebbea6e3741
Lumen-Lab <15043956+lumen-zluo@users.noreply.github.com>
2024-10-15 14:07:01 +0800

    Update test script

--- test_demo_video.py
+++ test_demo_video.py
@@ -3,12 +3,14 @@ import subprocess
 
 def main():
     command = [
-        'python', 'demo/long_video_demo.py',
+        "python",
+        "demo/demo.py",
         'configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb.py',
         'work_dirs/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb/epoch_50.pth',
-        'C://Users//Owner//Downloads//test.mp4',
+        'C:/Users/Owner/Downloads/test1.mp4',
         'tools/data/custom/label_map_custom.txt',
-        '../output/results.json'
+        "--out-filename",
+        "C:/Users/Owner/Downloads/demo_out.mp4"
     ]
 
     subprocess.run(command)
--- test_webcam_tsn.py
+++ test_webcam_tsn.py
@@ -6,7 +6,7 @@ def main():
         'python', 'demo/webcam_demo.py',
         # 'demo/demo_configs/tsn_r50_1x1x8_video_infer.py',
         'configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb.py',
-        'work_dirs/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb/epoch_100.pth',
+        'work_dirs/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb/epoch_50.pth',
         # 'tools/data/kinetics/label_map_k400.txt',
         'tools/data/custom/label_map_custom.txt',
         '--average-size', '5',
